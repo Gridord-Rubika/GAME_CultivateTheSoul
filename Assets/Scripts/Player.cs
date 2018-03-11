67123b86dd551dd0c815dfe4adeb6658d638ba70
Steve <s.delgrange@rubika-edu.com>
2018-03-11 23:21:08 +0100

    Game scene with shitty graphics but good enough.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player {
-    public enum CultivationTechniques
-    {
-        MORTAL = 0,
-        EARTH,
-        HEAVEN,
-        IMMORTAL
-    }
 
     public string id;
     public int soulLevel;
     public int soulForce;
-    public int soulStone;
-    public CultivationTechniques cultivationTechnique;
-    public DateTime lastTimeFarmed;
 }
